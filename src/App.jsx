870b5098f974fab0388b98122570651ae6fa3e72
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-12-22 23:09:44 +0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,27 @@ import ImageList from './components/ImageList';
 import searchImages from './api';
 
 export default function App() {
-  const [images, setImages] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (term) => {
-    setIsLoading(true)
+    setIsLoading(true);
     const result = await searchImages(term);
-    setImages(result)
-    console.log(result)
-    setIsLoading(false)
-  }
+    setImages(result);
+    console.log(result);
+    setIsLoading(false);
+  };
   return (
     <div>
-      {!isLoading ?
+      <SearchBar onSubmit={handleSubmit} />
+      <hr />
+      {!isLoading ? (
         <>
-          <SearchBar onSubmit={handleSubmit} />
-          <hr />
           <ImageList images={images} />
-        </> :
+        </>
+      ) : (
         <p>Loading...</p>
-      }
+      )}
     </div>
   );
 }
